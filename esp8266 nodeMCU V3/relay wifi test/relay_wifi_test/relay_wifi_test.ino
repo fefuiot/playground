691961f2b0d97e1725426de208103b6dd4ff1149
kutselabskii <osutanfirst@gmail.com>
2018-11-06 14:58:06 +1000

    Change led switch to tape switch

--- esp8266 nodeMCU V3/relay wifi test/relay_wifi_test/relay_wifi_test.ino
+++ esp8266 nodeMCU V3/relay wifi test/relay_wifi_test/relay_wifi_test.ino
@@ -50,10 +50,10 @@ void loop()
   server.handleClient();
 }
 
-void handleLED() {                          // If a POST request is made to URI /LED
-  digitalWrite(LED, !digitalRead(LED));     // Change the state of the LED
-  server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
-  server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+void handleLED() {                                          // If a POST request is made to URI /LED
+  digitalWrite(TAPE_SIGNAL, !digitalRead(TAPE_SIGNAL));     // Change the state of the tape
+  server.sendHeader("Location","/");                        // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
 }
 
 void handleRoot() {
