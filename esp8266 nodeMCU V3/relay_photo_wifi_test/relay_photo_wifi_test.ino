d2d89f5d7eec1d47a8d150e890090f9d332ef841
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2018-11-06 15:22:40 +1000

    [Photo Resistor][ESP8266] Add toggle tape button

--- esp8266 nodeMCU V3/relay_photo_wifi_test/relay_photo_wifi_test.ino
+++ esp8266 nodeMCU V3/relay_photo_wifi_test/relay_photo_wifi_test.ino
@@ -16,6 +16,47 @@ ESP8266WebServer server(80);
 bool noLight = false;
 bool isOn = false;
 
+String html = 
+"<!DOCTYPE html><html>"
+    "<head>"
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            "<link rel=\"icon\" href=\"data:,\">"
+                "<style>"
+                    "html" 
+                    "{"
+                    "    font-family: Helvetica;"
+                    "    display: inline-block;" 
+                    "    margin: 0px auto;"
+                    "    text-align: center;"
+                    "}"
+                    ".button"
+                    "{"
+                    "    background-color: #195B6A;"
+                    "    border: none;"
+                    "    color: white;" +
+                    "    padding: 16px 40px;"
+                    "    text-decoration: none;"
+                    "    font-size: 30px;"
+                    "    margin: 2px;"
+                    "    cursor: pointer;"
+                    "}"
+                    ".button2"
+                    "{"
+                    "    background-color: #77878A;"
+                    "}"
+                "</style>"
+    "</head>"
+    "<body>"
+        "<h1>ESP8266 Web Server</h1>"
+        "<p>LED Tape</p>"
+        "<p>"
+            "<a href=\"/LED\">"
+                "<button type=\"submit\" class=\"button\" form action=\"/Tape\" method=\"POST\">Toggle</button>"
+            "</a>"
+        "</p>"
+    "</body>"
+"</html>";
+
 void setup() 
 {
   pinMode(TAPE_SIGNAL, OUTPUT);
@@ -43,8 +84,7 @@ void setup()
   WiFi.printDiag(Serial);
 
   server.on("/", handleRootPath);
-  server.on("/on", tapeOn);
-  server.on("/off", tapeOff);
+  server.on("/Tape", handleToggleButton);
   server.begin();
   Serial.println("Server is on");
 }
@@ -62,21 +102,15 @@ void loop()
   delay(10);
 }
 
-void tapeOn()
-{
-  digitalWrite(TAPE_SIGNAL, LOW);
-  server.send(200, "text/plain", "Tape on!");
-}
-
-void tapeOff()
-{
-  digitalWrite(TAPE_SIGNAL, HIGH);
-  server.send(200, "text/plain", "Tape off!");
-}
-
 void handleRootPath()
 {
-  server.send(200, "text/plain", "Hello");
+  server.send(200, "text/plain", html); 
+}
+
+void handleToggleButton() {
+  isOn = !isOn;
+  server.sendHeader("Location", "/");
+  server.send(303);
 }
 
 void handleLight()
@@ -86,4 +120,3 @@ void handleLight()
   else
     digitalWrite(TAPE_SIGNAL, HIGH);
 }
-
